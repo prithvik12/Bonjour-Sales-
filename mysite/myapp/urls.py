5b013a9b4e0e0d6d3012bb353968f74784fd3112
akashELXSI <47232087+akashELXSI@users.noreply.github.com>
2022-02-26 12:38:50 -0500

    Code formatting

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path,include
 from . import views
 from myapp import views
 urlpatterns = [
-
     path('', views.index,name="index"),
     path('signup', views.signup,name="signup"),
     path('signin', views.signin,name="signin"),
