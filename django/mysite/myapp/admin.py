390cbb4f142cd8e8726297ea19faa7b1908c5466
Kausthuba Vanam <62927978+kausthuba-tech@users.noreply.github.com>
2022-03-28 18:23:09 -0400

    Add files via upload

--- django/mysite/myapp/admin.py
+++ django/mysite/myapp/admin.py
@@ -6,6 +6,13 @@ class PostAdminSite(admin.AdminSite):
     index_title = "Welcome to Bonjour Sales"
 
 post_admin_site = PostAdminSite(name='post_admin')
+admin.site.site_header = "Bonjour Sales Admin "
+admin.site.site_title = "Bonjour Sales Admin Portal"
+admin.site.index_title = "Welcome to Bonjour Sales"
 # Register your models here.
 admin.site.register(EMPLOYEE)
+admin.site.register(STORES)
 post_admin_site.register(EMPLOYEE)
+
+
+
